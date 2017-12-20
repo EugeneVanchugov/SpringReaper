7b7b6813396ce9390b1ea9a3986e0399e70e52d6
Evgenii_Vanchugov <evgenii_vanchugov@epam.com>
2017-12-20 14:40:29 +0300

    pom and some postProcessors

---
+++ src/main/java/com/epam/reaper/classes/InjectRandomIntAnnotationBeanPostProcessor.java
@@ -0,0 +1,32 @@
+package com.epam.reaper.classes;
+
+import com.epam.reaper.annotations.InjectRandomInt;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.util.ReflectionUtils;
+
+import java.lang.reflect.Field;
+import java.util.Random;
+
+public class InjectRandomIntAnnotationBeanPostProcessor implements BeanPostProcessor{
+    public Object postProcessBeforeInitialization(Object o, String s) throws BeansException {
+        Field[] fields = o.getClass().getDeclaredFields();
+        for (Field f :
+                fields) {
+            InjectRandomInt annotation = f.getAnnotation(InjectRandomInt.class);
+            if (annotation != null) {
+                int min = annotation.min();
+                int max = annotation.max();
+                Random random = new Random();
+                int i = min + random.nextInt(max - min);
+                f.setAccessible(true);
+                ReflectionUtils.setField(f, o, i);
+            }
+        }
+        return o;
+    }
+
+    public Object postProcessAfterInitialization(Object o, String s) throws BeansException {
+        return o;
+    }
+}
